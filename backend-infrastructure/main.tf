0b3afa18e961703b8312c220abbc03a644b345e1
Cristian Sulighetean <cristisulighetean@gmail.com>
2023-05-04 19:39:15 +0300

    Renamed the variables and moved the remote backend variables to backend.tfvars

--- backend-infrastructure/main.tf
+++ backend-infrastructure/main.tf
@@ -7,10 +7,6 @@ terraform {
   #############################################################
 
   backend "s3" {
-    bucket         = var.s3_bucket_name
-    key            = var.remote_state_key
-    region         = var.region
-    dynamodb_table = var.dynamodb_table_name
     encrypt        = true
   }
 
@@ -29,7 +25,7 @@ provider "aws" {
 
 # Create an S3 bucket to store Terraform state files
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.s3_bucket_name
+  bucket = var.bucket
   force_destroy = true
 }
 
@@ -53,7 +49,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 
 # Create a DynamoDB table for state file locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.dynamodb_table_name
+  name         = var.dynamodb_table
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
--- backend-infrastructure/variables.tf
+++ backend-infrastructure/variables.tf
@@ -4,20 +4,14 @@ variable "region" {
   default = "eu-north-1"
 }
 
-variable "s3_bucket_name" {
+variable "bucket" {
   description = "The name of the S3 bucket used for storing Terraform state files"
   type = string
   default = "devops-web-app-data-demo"
 }
 
-variable "dynamodb_table_name" {
+variable "dynamodb_table" {
   description = "The name of the DynamoDB table used for state file locking"
   type = string
   default = "terraform-state-locking"
-}
-
-variable "remote_state_key" {
-  description = "The S3 object key for the Terraform remote state file"
-  type = string
-  default = "demo-project/import-bootstrap/terraform.tfstate"
 }
