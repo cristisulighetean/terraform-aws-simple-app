a877f685512b4e9bcb11ee3396818a31b3a02ce9
Cristian Sulighetean <cristisulighetean@gmail.com>
2023-05-04 13:07:40 +0300

    Added support for remote backend on AWS

---
+++ aws-backend/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  #############################################################
+  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
+  ## UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
+  ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
+  #############################################################
+
+  backend "s3" {
+    bucket         = var.s3_bucket_name
+    key            = "demo-project/import-bootstrap/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = var.dynamodb_table_name
+    encrypt        = true
+  }
+
+  # Configure the backend to use AWS S3 for storing the TF state
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create an S3 bucket to store Terraform state files
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+  force_destroy = true
+}
+
+# Enable versioning on the S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption on the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Create a DynamoDB table for state file locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ aws-backend/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  default = "eu-north-1"
+  description = "The AWS region where resources will be created"
+}
+
+variable "s3_bucket_name" {
+  default = "devops-web-app-data-tf-demo"
+  description = "The name of the S3 bucket used for storing Terraform state files"
+}
+
+variable "dynamodb_table_name" {
+  default = "terraform-state-locking"
+  description = "The name of the DynamoDB table used for state file locking"
+}
