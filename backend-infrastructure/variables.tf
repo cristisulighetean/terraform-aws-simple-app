e959f699d107708c333560c089421a45fb858297
Cristian Sulighetean <cristisulighetean@gmail.com>
2023-05-06 21:57:26 +0300

    Changed the variables that are the input to the backend infrastructure due to the additon of workspace management

--- backend-infrastructure/variables.tf
+++ backend-infrastructure/variables.tf
@@ -1,13 +1,18 @@
-variable "region" {
-  description = "The AWS region where resources will be created"
-  type = string
-  default = "eu-north-1"
-}
-
 variable "bucket" {
   description = "The name of the S3 bucket used for storing Terraform state files"
   type = string
   default = "devops-web-app-data-demo"
+}
+
+variable "key" {
+  description = "The name of the S3 key used for storing Terraform state files"
+  type = "string"
+}
+
+variable "region" {
+  description = "The AWS region where resources will be created"
+  type = string
+  default = "eu-north-1"
 }
 
 variable "dynamodb_table" {
