a68ca2b411eb4a88d38041aad8f1942ac7b04688
Cristian Sulighetean <cristisulighetean@gmail.com>
2023-05-06 22:05:59 +0300

    Added the variables that are meant for the remote backend

--- web-app/variables.tf
+++ web-app/variables.tf
@@ -1,4 +1,20 @@
 # General variables
+variable "bucket" {
+  description = "The name of the S3 bucket used for storing Terraform state files"
+  type = string
+  default = "devops-web-app-data-demo"
+}
+
+variable "key" {
+  description = "The name of the S3 key used for storing Terraform state files"
+  type = "string"
+}
+
+variable "dynamodb_table" {
+  description = "The name of the DynamoDB table used for state file locking"
+  type = string
+  default = "terraform-state-locking"
+}
 variable "region" {
   description = "The AWS region where resources will be created"
   type = string
